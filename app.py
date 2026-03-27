e0669b6e03f5efe64292dff3cb5215d1869c4312
codewithyasho <yashodeephundiwale@gmail.com>
2026-03-28 02:43:46 +0530

    Implement FastAPI setup with CORS configuration and chatbot query handling

--- app.py
+++ app.py
@@ -1,2 +1,45 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from main import main
 
 
+# ========== Initialize FastAPI ==========
+app = FastAPI(
+    title="Yashodeep's Personal Chatbot API",
+    description="A RAG-based chatbot that answers questions about Yashodeep Hundiwale's skills, education, experience and etc.",
+    version="1.0.0"
+)
+
+# ========== Configure CORS ==========
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+class QueryRequest(BaseModel):
+    query: str = Field(..., description="The user's query to the chatbot.", examples=[
+                       "What is Yashodeep's educational background?"])
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Yashodeep's Personal Chatbot API! Use the /chat endpoint to interact with the chatbot."}
+
+
+@app.post("/chat")
+async def chat(query_request: QueryRequest):
+    try:
+        rag_chain = main()
+        response = rag_chain.invoke({
+            "input": query_request.query
+        })
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"answer": response["answer"]}
